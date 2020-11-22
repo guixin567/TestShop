c4cbe938806b6dcb1d8451bfd4949501b35e3b5b
zhengxin <xinzheng@shein.com>
2020-11-22 22:50:44 +0800

    feat(分类):完成页面逻辑 并增加缓存

--- pages/category/category.js
+++ pages/category/category.js
@@ -10,6 +10,10 @@ Page({
     leftMenuList:[],
     //右侧的分类数据
     rightCategoryList:[],
+    //左侧菜单选中的索引
+    selectMenuIndex:0,
+    //每次点击左侧菜单 回到顶部
+    scrollToTop:0,
   },
 
   //接口的返回的数据
@@ -18,14 +22,41 @@ Page({
 
   //生命周期函数--监听页面加载
   onLoad: function (options) {
+    //缓存不为空 并且时间没有过期 使用缓存，其他直接调用接口
+    const categoryCache = wx.getStorageSync('category_cache')
+    let cacheTime = 5 * 60 * 1000 
+    if(categoryCache && Date.now() - categoryCache.time < cacheTime){
+      console.log("缓存"+ Date.now() + categoryCache.time)
+      this.setData({
+        leftMenuList:categoryCache.data.map(v=>v.cat_name),
+        rightCategoryList:categoryCache.data[0].children
+      })
+    }else{
+      console.log("无缓存")
+      this.getCategoryData();  
+    }
+    
+  },
+
+  //获取分类接口
+  getCategoryData(){
     request({url:"https://api-hmugo-web.itheima.net/api/public/v1/categories"})
     .then(response=>{
-      console.log(response)
       this.categoryList = response.data.message,
+      wx.setStorageSync('category_cache', {time:Date.now(),data:this.categoryList})
       this.setData({
         leftMenuList:this.categoryList.map(v=>v.cat_name),
-        rightCategoryList:this.categoryList[0]
+        rightCategoryList:this.categoryList[0].children
       })
     })
   },
+  onMenuClick:function(event){
+    console.log(event)
+    let index = event.currentTarget.dataset.index
+    this.setData({
+      selectMenuIndex:index,
+      rightCategoryList:this.categoryList[index].children,
+      scrollToTop:0,
+    })
+  }
 })
